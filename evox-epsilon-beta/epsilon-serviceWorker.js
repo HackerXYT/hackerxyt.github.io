dda5a8bac643855f842bea9bbfccf1eed42d1e07
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2024-09-21 19:49:43 +0300

    Update epsilon-serviceWorker.js

--- evox-epsilon-beta/epsilon-serviceWorker.js
+++ evox-epsilon-beta/epsilon-serviceWorker.js
@@ -20,8 +20,8 @@ self.addEventListener('notificationclick', function (event) {
   );
 });
 
-const STATIC_CACHE_NAME = 'epsilon-cache-v18';
-const APP_CACHE_NAME = 'epsilon-app-cache-v18';
+const STATIC_CACHE_NAME = 'epsilon-cache-v19';
+const APP_CACHE_NAME = 'epsilon-app-cache-v19';
 const CACHE_STATIC = [
   '/evox-epsilon-beta/epsilon-frontend-assets/epsilon.png',
   '/evox-epsilon-beta/epsilon-frontend-assets/customize.png',
