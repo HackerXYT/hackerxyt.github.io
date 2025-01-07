3ccb5178620286f3186d10b89cdd86674ebadc93
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2025-01-07 02:12:12 +0200

    Some smalls

--- oasaResign/intelligence.js
+++ oasaResign/intelligence.js
@@ -56,8 +56,21 @@ function openSearch() {
 <path d="M17.481 17.8267C17.5684 17.8558 17.653 17.884 17.735 17.9113Z" fill="#fff"/>
 </svg>Σπίτι
                     </div>`
+  favoriteBuses.forEach(bus => {
+    document.getElementById("recommendSpawn").innerHTML += `<div onclick="spawnAndShowInfo('${bus}')" class="Block favorite"><svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14.5 19.9815C16.0728 19.9415 17.1771 19.815 18 19.4151V20.9999C18 21.5522 17.5523 21.9999 17 21.9999H15.5C14.9477 21.9999 14.5 21.5522 14.5 20.9999V19.9815Z" fill="#FFF"/>
+                  <path d="M6 19.415C6.82289 19.815 7.9272 19.9415 9.5 19.9815V20.9999C9.5 21.5522 9.05228 21.9999 8.5 21.9999H7C6.44772 21.9999 6 21.5522 6 20.9999V19.415Z" fill="#FFF"/>
+                  <path opacity="0.5" fill-rule="evenodd" clip-rule="evenodd" d="M5.17157 3.17157C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.17157C19.8915 4.23467 19.99 5.8857 19.9991 9L20 13C19.9909 16.1143 19.8915 17.7653 18.8284 18.8284C18.5862 19.0706 18.3136 19.2627 18 19.4151C17.1771 19.8151 16.0728 19.9415 14.5 19.9815C13.7729 19.9999 12.9458 20 12 20C11.0542 20 10.2271 20 9.5 19.9815C7.9272 19.9415 6.82289 19.815 6 19.415C5.68645 19.2626 5.41375 19.0706 5.17157 18.8284C4.10848 17.7653 4.00911 16.1143 4 13L4.00093 9C4.01004 5.8857 4.10848 4.23467 5.17157 3.17157Z" fill="#FFF"/>
+                  <path d="M17.75 16C17.75 15.5858 17.4142 15.25 17 15.25H15.5C15.0858 15.25 14.75 15.5858 14.75 16C14.75 16.4142 15.0858 16.75 15.5 16.75H17C17.4142 16.75 17.75 16.4142 17.75 16Z" fill="#FFF"/>
+                  <path d="M6.25 16C6.25 15.5858 6.58579 15.25 7 15.25H8.5C8.91421 15.25 9.25 15.5858 9.25 16C9.25 16.4142 8.91421 16.75 8.5 16.75H7C6.58579 16.75 6.25 16.4142 6.25 16Z" fill="#FFF"/>
+                  <path opacity="0.5" d="M5.5 9.5C5.5 10.9142 5.5 11.6213 5.93934 12.0607C6.37868 12.5 7.08579 12.5 8.5 12.5H15.5C16.9142 12.5 17.6213 12.5 18.0607 12.0607C18.5 11.6213 18.5 10.9142 18.5 9.5V6.99998C18.5 5.58578 18.5 4.87868 18.0607 4.43934C17.6213 4 16.9142 4 15.5 4H8.5C7.08579 4 6.37868 4 5.93934 4.43934C5.5 4.87868 5.5 5.58579 5.5 7V9.5Z" fill="#FFF"/>
+                  <path d="M2.4 11.8L4 13L4.00093 9H3C2.44772 9 2 9.44772 2 10V11C2 11.3148 2.14819 11.6111 2.4 11.8Z" fill="#FFF"/>
+                  <path d="M21 9H19.999L20 13L21.6 11.8C21.8518 11.6111 22 11.3148 22 11V10C22 9.44772 21.5522 9 21 9Z" fill="#FFF"/>
+                  </svg>${bus}
+                                      </div>`
+  })
   frequentBuses.forEach(bus => {
-    document.getElementById("recommendSpawn").innerHTML += `<div class="Block"><svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    document.getElementById("recommendSpawn").innerHTML += `<div onclick="spawnAndShowInfo('${bus}')" class="Block"><svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M14.5 19.9815C16.0728 19.9415 17.1771 19.815 18 19.4151V20.9999C18 21.5522 17.5523 21.9999 17 21.9999H15.5C14.9477 21.9999 14.5 21.5522 14.5 20.9999V19.9815Z" fill="#FFF"/>
 <path d="M6 19.415C6.82289 19.815 7.9272 19.9415 9.5 19.9815V20.9999C9.5 21.5522 9.05228 21.9999 8.5 21.9999H7C6.44772 21.9999 6 21.5522 6 20.9999V19.415Z" fill="#FFF"/>
 <path opacity="0.5" fill-rule="evenodd" clip-rule="evenodd" d="M5.17157 3.17157C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.17157C19.8915 4.23467 19.99 5.8857 19.9991 9L20 13C19.9909 16.1143 19.8915 17.7653 18.8284 18.8284C18.5862 19.0706 18.3136 19.2627 18 19.4151C17.1771 19.8151 16.0728 19.9415 14.5 19.9815C13.7729 19.9999 12.9458 20 12 20C11.0542 20 10.2271 20 9.5 19.9815C7.9272 19.9415 6.82289 19.815 6 19.415C5.68645 19.2626 5.41375 19.0706 5.17157 18.8284C4.10848 17.7653 4.00911 16.1143 4 13L4.00093 9C4.01004 5.8857 4.10848 4.23467 5.17157 3.17157Z" fill="#FFF"/>
@@ -69,6 +82,7 @@ function openSearch() {
 </svg>${bus}
                     </div>`
   })
+
   //andMore
 }
 
@@ -1540,7 +1554,14 @@ function processInfo(evoxId, type) {
                                             <div class="info">
                                                 <div class="text">
                                                     <span>Επόμενη άφιξη</span>
-                                                    <span id="station-${station.StopCode}"><div class="loaderTimes"></div></span>
+                                                    <span id="station-${station.StopCode}"><!--<div class="loaderTimes"></div>--><svg class="compassAnim" xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 24 24" fill="none">
+        <path opacity="0.5"
+            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+            fill="#8fceff" />
+        <path
+            d="M13.024 14.5601C13.5166 14.363 13.763 14.2645 13.9562 14.095C14.0055 14.0518 14.0518 14.0055 14.095 13.9562C14.2645 13.763 14.363 13.5166 14.5601 13.024C15.484 10.7142 15.946 9.5593 15.4977 8.89964C15.3914 8.74324 15.2565 8.60834 15.1001 8.50206C14.4405 8.0538 13.2856 8.51575 10.9758 9.43966C10.4831 9.63673 10.2368 9.73527 10.0435 9.90474C9.99429 9.94792 9.94792 9.99429 9.90474 10.0435C9.73527 10.2368 9.63673 10.4831 9.43966 10.9758C8.51575 13.2856 8.0538 14.4405 8.50206 15.1001C8.60834 15.2565 8.74324 15.3914 8.89964 15.4977C9.5593 15.946 10.7142 15.484 13.024 14.5601Z"
+            fill="#fff" />
+    </svg></span>
                                                 </div>
                                             </div>
                                             <div class="fav-actions">
@@ -1676,7 +1697,7 @@ function processInfo(evoxId, type) {
 
 function returnFromBusTimetable() {
   activeBusInfo = {}
-  if(document.getElementById("returnTopDefines").classList.contains("scrolled")) {
+  if (document.getElementById("returnTopDefines").classList.contains("scrolled")) {
     const element = document.getElementById('main-wrapper');
     element.scrollTop = 0;
   }
@@ -2145,4 +2166,75 @@ async function getStopArrivalTime(stopCode, stopName, cords, maxRetries = 5) {
     }
   }
   return []; // Return empty array if all retries fail
+}
+
+function spawnAndShowInfo(bus) {
+  //get the bus stops
+  findBusInfo2(bus).then((returned) => {
+    console.log(returned)
+    //then
+    const work = returned[0].route_code
+    fetch(`https://data.evoxs.xyz/proxy?key=21&targetUrl=https%3A%2F%2Ftelematics.oasa.gr%2Fapi%2F%3Fact%3DwebGetRoutesDetailsAndStops%26p1%3D${returned}%26keyOrigin%3DevoxEpsilon`)
+      .then(response => response.json())
+      .then(data => {
+        const coordinates = data.stops
+          .map((stop) => ({
+            lat: parseFloat(stop.StopLat),
+            lng: parseFloat(stop.StopLng),
+          }))
+          .sort((a, b) => a.RouteStopOrder - b.RouteStopOrder);
+
+        // Add markers and collect coordinates for the line
+        console.log(data.stops.length)
+        coordinates.forEach((coord, index) => {
+          // Create a custom HTML element for the marker
+          const dot = document.createElement('div');
+          if (index === data.stops.length - 1) {
+            console.log("Last station")
+            dot.className = "start-end"
+          } else {
+            dot.className = "custom-marker"
+          }
+          if (index === data.stops.length - 1) {
+            console.log("Last station")
+            dot.innerHTML = '<div class="marker-icon"><img src="evox-logo-beta-large.png"></div>'
+          }
+
+
+          new mapboxgl.Marker(dot)
+            .setLngLat([coord.lng, coord.lat])
+            .addTo(map);
+          console.log(index)
+        });
+
+        // Add a line connecting the points
+        map.addSource("route", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            geometry: {
+              type: "LineString",
+              coordinates: coordinates.map((coord) => [coord.lng, coord.lat]),
+            },
+          },
+        });
+
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: "route",
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#007cbf",
+            "line-width": 4,
+          },
+        });
+
+
+      })
+      .catch(error => console.error('Error:', error));
+  })
 }
