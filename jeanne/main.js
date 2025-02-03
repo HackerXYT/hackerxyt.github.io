75685e51ae380ec1cda35dba8e80e3f2da334738
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2025-02-03 17:20:31 +0200

    Discover Feed

--- jeanne/main.js
+++ jeanne/main.js
@@ -115,7 +115,7 @@ function selectCustom(name) {
                     setTimeout(function () {
                         const a = foundName.split(' ')[0].replace(/[σς]+$/, '')
                         const b = foundName.split(' ')[1].replace(/[σς]+$/, '')
-                        document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0],0)} ${transformGreekName(matchedNames[0],1)}`
+                        document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0], 0)} ${transformGreekName(matchedNames[0], 1)}`
                         document.getElementById("loadText").style.opacity = '1'
                         setTimeout(function () {
                             document.getElementById("topImg").style.opacity = '0'
@@ -289,7 +289,7 @@ function find() {
                                 setTimeout(function () { //
                                     const a = matchedNames[0].split(' ')[0].replace(/[σς]+$/, '')
                                     const b = matchedNames[0].split(' ')[1].replace(/[σς]+$/, '')
-                                    document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0],0)} ${transformGreekName(matchedNames[0],1)}`
+                                    document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0], 0)} ${transformGreekName(matchedNames[0], 1)}`
                                     document.getElementById("loadText").style.opacity = '1'
                                     setTimeout(function () {
                                         document.getElementById("topImg").style.opacity = '0'
@@ -386,7 +386,7 @@ function find() {
                             setTimeout(function () { //
                                 const a = matchedNames[0].split(' ')[0].replace(/[σς]+$/, '')
                                 const b = matchedNames[0].split(' ')[1].replace(/[σς]+$/, '')
-                                document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0],0)} ${transformGreekName(matchedNames[0],1)}`
+                                document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0], 0)} ${transformGreekName(matchedNames[0], 1)}`
                                 document.getElementById("loadText").style.opacity = '1'
                                 setTimeout(function () {
                                     document.getElementById("topImg").style.opacity = '0'
@@ -509,12 +509,12 @@ function continueToLogin(ev) {
 }
 function hideElementOnAndroid(elementId) {
     if (navigator.userAgent.toLowerCase().includes('android')) {
-      const element = document.getElementById(elementId);
-      if (element) {
-        element.style.display = 'none';
-      }
+        const element = document.getElementById(elementId);
+        if (element) {
+            element.style.display = 'none';
+        }
     }
-  }
+}
 
 
 let namesData = null
@@ -1121,7 +1121,7 @@ function autoLogin() {
 
 function transformGreekName(name, num) {
     const firstName = name.split(' ')[num].replace(/[σς]+$/, ''); // Remove trailing σ/ς
-    
+
     let transformedName = firstName;
 
     if (firstName.endsWith("ος")) {
@@ -1161,7 +1161,7 @@ function attach() {
 
             document.getElementById("emri").innerText = `${transformGreekName(foundName, 0)}`
         } else {
-            document.getElementById("emri").innerText = `${transformGreekName(foundName,0)} ${transformGreekName(foundName,1)}`
+            document.getElementById("emri").innerText = `${transformGreekName(foundName, 0)} ${transformGreekName(foundName, 1)}`
         }
 
         if (!sessionStorage.getItem('isNewUser')) {
@@ -1383,7 +1383,7 @@ function activateYearbook() {
                             console.log(classes);
 
                             Object.entries(classes).forEach(([key, aclass]) => {
-                                if (!document.getElementById("spawnPeople").innerText.includes(key)) {
+                                if (!document.getElementById("spawnPeople").innerText.includes(key) && key !== "ΚΑΘnone") {
                                     const toFind = key.match(/[Α-Ω]+|\d+/g);
                                     document.getElementById("spawnPeople").innerHTML += `<div class="spawnPeople" id="${key}-cont">
                                     <p style="text-align: left">${toFind[0]}${toFind[1] ? "'" + toFind[1] : ""}</p></div>` //${key.includes("ΚΑΘ") ? " style='display: none'" : ""}
@@ -1394,15 +1394,17 @@ function activateYearbook() {
 
 
                                 // key -> class, user -> data
-                                if (key === selfClass) {
+                                if (key === selfClass && key !== "ΚΑΘnone") {
 
                                     document.getElementById(`${key}-cont`).classList.add("upup")
                                     document.getElementById(`${key}-cont`).innerText = 'Στην τάξη σου'
                                     //classes[`${user.seksioni}${user.klasa}`].push(user);
+                                } else if (selfClass === "ΚΑΘnone" && !document.getElementById("spawnPeople").innerHTML.includes(`<p>⚠️ Προσοχή!`)) {
+                                    document.getElementById("spawnPeople").innerHTML = `<p>⚠️ Προσοχή!<br>Οι χρήστες της τάξης σας είναι κρυμμένοι.</p>${document.getElementById("spawnPeople").innerHTML}`
                                 }
 
                                 Object.entries(aclass).forEach(([nameEach, inform]) => {
-                                    if (inform.emri === foundName) { return; }
+                                    if (inform.emri === foundName || key === "ΚΑΘnone") { return; }
                                     const ranId = Math.floor(Math.random() * 909999) + 1
                                     usersElems[inform.emri] = { ranId: ranId, info: inform }
                                     document.getElementById(`${key}-cont`).innerHTML += `<div id="user-${ranId}" class="aStudent fade-in-slide-up" onclick="pickStudent('${inform.emri}', this)">
@@ -1837,12 +1839,15 @@ async function getEvoxProfile(name) {
             }
             const regex = /\/([^\/]+)\.evox$/;
             const match = data.match(regex);
+            console.log(match)
             if (match) {
                 const extracted = match[1];
                 socialUsername = extracted;
                 document.getElementById("instausername-SELF").innerText = socialUsername
+                document.getElementById("isInstagramed").style.display = null
             } else {
                 console.log("No match found");
+                document.getElementById("isInstagramed").style.display = 'none'
             }
             return data;
         } else {
@@ -2255,14 +2260,14 @@ function goBackToMain() {
 
 function getHelpSend() {
     fetch(`https://arc.evoxs.xyz/?metode=needHelp&emri=${document.getElementById("getHelpInput").value}`)
-                .then(response => response.text())
-                .then(text => {
-                    document.getElementById("getHelpInput").value = ''
-                    goBackToMain()
+        .then(response => response.text())
+        .then(text => {
+            document.getElementById("getHelpInput").value = ''
+            goBackToMain()
 
-                }).catch(error => {
-                    
-                });
+        }).catch(error => {
+
+        });
 }
 
 function goBackFromHelp() {
@@ -2412,7 +2417,7 @@ function searchByNameComplete() {
             //setTimeout(function () { //
             const a = matchedNames[0].split(' ')[0].replace(/[σς]+$/, '')
             const b = matchedNames[0].split(' ')[1].replace(/[σς]+$/, '')
-            document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0],0)} ${transformGreekName(matchedNames[0],1)}`
+            document.getElementById("loadText").innerHTML = `Καλωσόρισες,<br>${transformGreekName(matchedNames[0], 0)} ${transformGreekName(matchedNames[0], 1)}`
             document.getElementById("loadText").style.opacity = '1'
             setTimeout(function () {
                 document.getElementById("topImg").style.opacity = '0'
@@ -2565,7 +2570,7 @@ function YbsearchByName() {
         //if (usersElems[`'${part}'`]) {
         //console.log(document.getElementById(`searchPeople`).innerHTML, part, el.innerHTML.includes(part))
         console.log(spawnedSearches, part)
-        if(part === foundName && matchedNames.length === 1) {
+        if (part === foundName && matchedNames.length === 1) {
             el.innerHTML += 'Δεν μπορείτε να γράψετε για τον εαυτό σας'
             return;
         }
@@ -2614,7 +2619,7 @@ function YbsearchByName() {
         }, 200)
 
     });
-    if(matchedNames.length === 0) {
+    if (matchedNames.length === 0) {
         el.innerHTML += 'Δεν βρέθηκαν αντιστοιχίες'
     }
     const firstName = queryParts[0];
@@ -2818,4 +2823,93 @@ function moreOptions(el) {
 
 function startEvoxLogin() {
     window.location.href = '../evox-epsilon-beta/?metode=jeandarc'
+}
+
+function updateProgress(percentage) {
+    const circle = document.querySelector('.circle-fill');
+    const maxStroke = 188; // The full circumference of the circle
+    const offset = maxStroke - (maxStroke * percentage) / 100;
+    
+    circle.style.strokeDashoffset = offset;
+}
+
+function openDiscovery(el) {
+    el.classList.add('active')
+    document.getElementById("home-switch").classList.remove("active")
+    document.getElementById("home").style.display = 'none'
+    document.getElementById("discover").style.display = 'block'
+    fetch('https://arc.evoxs.xyz/?metode=progresin')
+        .then(response => response.json())
+        .then(progress_global => {
+            const progress = progress_global.global
+            document.getElementById("countDone").innerHTML = progress.have_participated
+            document.getElementById("countFull").innerHTML = progress.total_users
+            document.getElementById("countLeft").innerHTML = progress.total_users - progress.have_participated
+            const percentage = Number.parseInt(100*progress.have_participated/progress.total_users)
+            document.getElementById("isDone").innerHTML = percentage + "%"
+            updateProgress(percentage);
+            const progress_class = progress_global.byclass
+            document.getElementById("classes").innerHTML = ''
+            Object.entries(progress_class.class_counts).forEach(([key, value]) => {
+                if(key === 'ΚΑΘ'){return;}
+                document.getElementById("classes").innerHTML += `<div class="aclass">
+                    <div class="left">
+                    ${key === "ΓΥΓ"? "Υγείας" : key.includes("ΓΑΝΘ1") ? "Θεωρητ. 1" : key === 'ΓΟΠ1' ? "Οικον. 1" : key === 'ΓΟΠ2' ? "Οικον. 2" : key === "ΓΑΝΘ2" ? "Θεωρητ. 2" : key === "ΓΘΤ" ? "Θετικών" : key}
+                    <p>${value.have_participated}<vox class="smallto">/${value.total}</vox></p>
+                    </div>
+                    <div class="right">
+                        ${key === 'ΓΥΓ' ? `<svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 32 32">
+
+<defs>
+
+<style>.cls-1{fill:#e291a8;}.cls-2{fill:#dd2853;}</style>
+
+</defs>
+
+<title/>
+
+<g data-name="Layer 22" id="Layer_22">
+
+<path class="cls-1" d="M27.31,7.54a8,8,0,0,0-5.64-2.3h-.09A8,8,0,0,0,16,7.58a8.2,8.2,0,0,0-5.71-2.26,8.17,8.17,0,0,0-5.62,14l.43.39a1.07,1.07,0,0,0,.14.16l10.22,10a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.3l10-10.23a1.06,1.06,0,0,0,.24-.39l.32-.31a8.08,8.08,0,0,0-.12-11.39Z"/>
+
+<path class="cls-2" d="M29,15.13,18.26,15h0a1,1,0,0,0-.92.61L17,14.28a1,1,0,0,0-.61-.67,1,1,0,0,0-.91.11l-1.26.85-1.4-4a1,1,0,0,0-.8-.66,1,1,0,0,0-1,.4L7.57,15,3,15.13a1,1,0,0,0,.06,2L8.11,17a1,1,0,0,0,.78-.41L11.52,13l1.2,3.45a1,1,0,0,0,.63.62,1,1,0,0,0,.88-.12l1.18-.8.79,2.9a1,1,0,0,0,.92.74h.05a1,1,0,0,0,.93-.64L18.93,17,29,17.13h0a1,1,0,0,0,0-2Z"/>
+
+</g>
+
+</svg>`: key.includes('ΓΑΝΘ') ? `<svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" viewBox="0 0 512 512" xml:space="preserve">
+<polygon style="fill:#B4B4B4;" points="435.298,32.603 256,32.603 235.102,272.301 256,512 435.298,512 "/>
+<rect x="76.706" y="32.601" style="fill:#E0E0E0;" width="179.294" height="479.399"/>
+<g>
+	<rect x="150.183" y="103.424" style="fill:#707070;" width="211.634" height="31.347"/>
+	<rect x="150.674" y="161.061" style="fill:#707070;" width="31.347" height="308.987"/>
+	<rect x="240.327" y="161.061" style="fill:#707070;" width="31.347" height="308.987"/>
+	<rect x="329.979" y="161.061" style="fill:#707070;" width="31.347" height="308.987"/>
+</g>
+<polygon style="fill:#424242;" points="446.794,0 256,0 235.102,32.603 256,65.206 446.794,65.206 "/>
+<rect x="65.202" style="fill:#707070;" width="190.798" height="65.202"/>
+<path style="fill:#B4B4B4;" d="M65.206,0L44.308,65.206l20.898,65.206c36.012,0,65.206-29.193,65.206-65.206  C130.411,29.193,101.217,0,65.206,0z"/>
+<path style="fill:#E0E0E0;" d="M0,65.206c0,36.012,29.193,65.206,65.206,65.206V0C29.193,0,0,29.193,0,65.206z"/>
+<path style="fill:#424242;" d="M65.206,40.774L54.757,65.206l10.449,24.432c13.493,0,24.432-10.938,24.432-24.432  C89.637,51.712,78.699,40.774,65.206,40.774z"/>
+<path style="fill:#707070;" d="M40.774,65.206c0,13.493,10.938,24.432,24.432,24.432V40.774  C51.712,40.774,40.774,51.712,40.774,65.206z"/>
+<path style="fill:#B4B4B4;" d="M446.794,0l-20.898,65.206l20.898,65.206c36.012,0,65.206-29.193,65.206-65.206S482.807,0,446.794,0z  "/>
+<path style="fill:#E0E0E0;" d="M381.589,65.206c0,36.012,29.193,65.206,65.206,65.206V0C410.783,0,381.589,29.193,381.589,65.206z"/>
+<path style="fill:#424242;" d="M446.794,40.774l-10.449,24.432l10.449,24.432c13.493,0,24.432-10.938,24.432-24.432  S460.288,40.774,446.794,40.774z"/>
+<path style="fill:#707070;" d="M422.363,65.206c0,13.493,10.938,24.432,24.432,24.432V40.774  C433.301,40.774,422.363,51.712,422.363,65.206z"/>
+</svg>`: key.includes("ΓΟΠ") ? `<svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 24 24" fill="none">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM10.6158 9.5C11.0535 8.71823 11.8025 8 12.7498 8C13.284 8 13.819 8.23239 14.2923 8.70646C14.6824 9.09734 15.3156 9.09792 15.7065 8.70775C16.0973 8.31758 16.0979 7.68442 15.7077 7.29354C14.9274 6.51179 13.9042 6 12.7498 6C11.3289 6 10.1189 6.77025 9.29826 7.86449C8.93769 8.34528 8.64329 8.89783 8.42654 9.5H8C7.44772 9.5 7 9.94772 7 10.5C7 10.9581 7.30804 11.3443 7.72828 11.4626C7.82228 11.4891 7.91867 11.5 8.01613 11.5C7.99473 11.8304 7.99473 12.1696 8.01613 12.5C7.91867 12.5 7.82228 12.5109 7.72828 12.5374C7.30804 12.6557 7 13.0419 7 13.5C7 14.0523 7.44772 14.5 8 14.5H8.42654C8.64329 15.1022 8.93769 15.6547 9.29826 16.1355C10.1189 17.2298 11.3289 18 12.7498 18C13.9042 18 14.9274 17.4882 15.7077 16.7065C16.0979 16.3156 16.0973 15.6824 15.7065 15.2923C15.3156 14.9021 14.6824 14.9027 14.2923 15.2935C13.819 15.7676 13.284 16 12.7498 16C11.8025 16 11.0535 15.2818 10.6158 14.5H12C12.5523 14.5 13 14.0523 13 13.5C13 12.9477 12.5523 12.5 12 12.5H10.0217C9.99312 12.1735 9.99312 11.8265 10.0217 11.5H13C13.5523 11.5 14 11.0523 14 10.5C14 9.94772 13.5523 9.5 13 9.5H10.6158Z" fill="#fff"/>
+</svg>` : key.includes("ΓΘΤ") ? `<svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 1024 1024" class="icon" version="1.1"><path d="M857.7 583.1c-6.7-11.8-21.8-15.8-33.5-9-11.8 6.7-15.8 21.8-9.1 33.5 66.6 115.9 83.4 212.6 43.8 252.2-75.7 75.8-311.6-54.5-476-218.9-41.5-41.5-78.8-84.7-111.3-127.9 33.4-45.1 71.3-89.2 111.3-129.2C547.2 219.5 783.1 89.3 858.9 165c30.9 30.9 27.7 97.6-8.9 183-40.1 93.6-114.7 197.7-210 293-22.3 22.3-45.4 43.8-68.7 63.8-10.3 8.8-11.4 24.4-2.6 34.6 8.9 10.3 24.4 11.4 34.6 2.6 24.2-20.8 48.2-43.2 71.4-66.3 99.6-99.6 177.9-209.1 220.4-308.3 45.6-106.3 45-190.5-1.5-237C802 38.8 562.4 135 348.2 349.3c-39.9 39.9-75.7 80.7-107 121.2-28.1-41.7-51.4-83-68.3-122.4-36.6-85.3-39.8-152-8.9-183 39.6-39.6 136.1-22.9 252 43.6 11.7 6.7 26.8 2.7 33.5-9.1 6.7-11.8 2.7-26.8-9.1-33.5-140-80.3-253.4-93.4-311.1-35.7-46.6 46.6-47.1 130.7-1.5 237 20 46.8 48.2 95.8 82.6 145C97.5 674.2 60.7 825.9 129.3 894.5c23.8 23.8 57 35.5 97.6 35.5 58.7 0 132.9-24.6 216.5-73 11.7-6.8 15.7-21.8 8.9-33.6-6.8-11.7-21.8-15.7-33.6-8.9-117.1 68-214.7 85.3-254.7 45.3-51.6-51.6-7.5-177.6 77.8-304.7 31.6 40.9 67.3 81.5 106.3 120.5 99.6 99.6 209.1 177.8 308.4 220.4 52.5 22.5 99.7 33.8 139.6 33.8 40.8 0 73.9-11.8 97.5-35.3 57.7-57.7 44.6-171.2-35.9-311.4zM511.5 430.5c-45.2 0-81.9 36.7-81.9 81.9s36.7 81.9 81.9 81.9 81.9-36.7 81.9-81.9c-0.1-45.2-36.7-81.9-81.9-81.9z" fill="#FFF"/></svg>`: "error"}
+                    </div>
+                </div>`
+                console.log(`Class: ${key}, Total: ${value.total}, Participated: ${value.have_participated}`);
+            });
+        }).catch(error => {
+            console.log('Error:', error);
+        });
+}
+
+function openHome(el) {
+    el.classList.add('active')
+    document.getElementById("discovery-switch").classList.remove("active")
+    document.getElementById("home").style.display = 'block'
+    document.getElementById("discover").style.display = 'none'
 }
