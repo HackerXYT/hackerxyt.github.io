3d0c6d8a0f289fe194686f052cf02898d56cd0ab
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2025-02-14 16:13:37 +0200

    Numerical V: epsilon-serviceWorker

--- evox-epsilon-beta/epsilon-serviceWorker.js
+++ evox-epsilon-beta/epsilon-serviceWorker.js
@@ -20,8 +20,8 @@ self.addEventListener('notificationclick', function (event) {
   );
 });
 
-const STATIC_CACHE_NAME = 'epsilon-cache-v36';
-const APP_CACHE_NAME = 'epsilon-app-cache-v36';
+const STATIC_CACHE_NAME = 'epsilon-cache-v37';
+const APP_CACHE_NAME = 'epsilon-app-cache-v37';
 const CACHE_STATIC = [
   '/evox-epsilon-beta/epsilon-frontend-assets/epsilon.png',
   '/evox-epsilon-beta/epsilon-frontend-assets/customize.png',
