694d7d2214e0df76b1974d91027ea52b5a427a82
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2025-03-16 16:30:22 +0200

    some

--- jeanneBeta/main.js
+++ jeanneBeta/main.js
@@ -1310,7 +1310,7 @@ function attach() {
         //}, 500)
 
     }
-    document.body.style.backgroundColor = 'rgb(5,2,16)'
+    document.body.style.backgroundColor = '#101010'//'rgb(5,2,16)'
     $("#hexa").fadeOut("fast", function () {
         $("#tasks").fadeOut("fast")
 
@@ -3435,6 +3435,87 @@ function timeAgoInGreek(isoDate) {
 }
 
 function loadSentByUser() {
+    document.getElementById("sentByUser").innerHTML = `<div class="postContainer skel loading">
+                        <div class="post">
+                            <div style="display: flex;flex-direction: row;align-items:center;">
+                                <div class="profilePicture">
+                                    <span style="background-color: #1c1c1c;width: 45px;height: 45px;border-radius: 50%;">
+                                </div>
+                                <div class="postInfo">
+                                    <div class="userInfo">
+                                        <p class="skeleton"></p>
+                                        <span class="skeleton"></span>
+                                    </div>
+                                    <div class="postContent">
+                                        <p class="skeleton"></p>
+                                        <p style="margin-top: 5px;" class="skeleton"></p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="postContainer skel loading">
+                        <div class="post">
+                            <div style="display: flex;flex-direction: row;align-items:center;">
+                                <div class="profilePicture">
+                                    <span style="background-color: #1c1c1c;width: 45px;height: 45px;border-radius: 50%;">
+                                </div>
+                                <div class="postInfo">
+                                    <div class="userInfo">
+                                        <p class="skeleton"></p>
+                                        <span class="skeleton"></span>
+                                    </div>
+                                    <div class="postContent">
+                                        <p class="skeleton"></p>
+                                        <p style="margin-top: 5px;" class="skeleton"></p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="postContainer skel loading">
+                        <div class="post">
+                            <div style="display: flex;flex-direction: row;align-items:center;">
+                                <div class="profilePicture">
+                                    <span style="background-color: #1c1c1c;width: 45px;height: 45px;border-radius: 50%;">
+                                </div>
+                                <div class="postInfo">
+                                    <div class="userInfo">
+                                        <p class="skeleton"></p>
+                                        <span class="skeleton"></span>
+                                    </div>
+                                    <div class="postContent">
+                                        <p class="skeleton"></p>
+                                        <p style="margin-top: 5px;" class="skeleton"></p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="postContainer skel loading">
+                        <div class="post">
+                            <div style="display: flex;flex-direction: row;align-items:center;">
+                                <div class="profilePicture">
+                                    <span style="background-color: #1c1c1c;width: 45px;height: 45px;border-radius: 50%;">
+                                </div>
+                                <div class="postInfo">
+                                    <div class="userInfo">
+                                        <p class="skeleton"></p>
+                                        <span class="skeleton"></span>
+                                    </div>
+                                    <div class="postContent">
+                                        <p class="skeleton"></p>
+                                        <p style="margin-top: 5px;" class="skeleton"></p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>`
+
     const account_data = localStorage.getItem("jeanDarc_accountData")
     if (!account_data) {
         console.error("Llogaria nuk eshte ruajtur ne nivel lokal!?")
@@ -3443,8 +3524,8 @@ function loadSentByUser() {
 
     
     function spawnIn(sentbyuser, local) {
+        let html = ''
         
-        document.getElementById("sentByUser").innerHTML = ''
         Promise.all(
             sentbyuser.map(sent => 
                 getEvoxProfile(sent.marresi).then(pfp => {
@@ -3468,7 +3549,8 @@ function loadSentByUser() {
                         </div>
                     </div>`;
         
-                    document.getElementById("sentByUser").innerHTML += ready;
+                    html += ready
+                    
                 })
             )
         ).then(() => {
@@ -3477,6 +3559,7 @@ function loadSentByUser() {
             if(local === true) {
                 loadFresh(true)
             }
+            document.getElementById("sentByUser").innerHTML = html;
         });
         
     }
@@ -3788,7 +3871,7 @@ function openSearch(el, inBackground) {
                     <div class="postInfo">
                         <div class="userInfo">
                             <p>${info.emri} 
-                            ${info.seksioni === 'ΚΑΘ' ? '<svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" class="icon glyph"><path d="M21.6,9.84A4.57,4.57,0,0,1,21.18,9,4,4,0,0,1,21,8.07a4.21,4.21,0,0,0-.64-2.16,4.25,4.25,0,0,0-1.87-1.28,4.77,4.77,0,0,1-.85-.43A5.11,5.11,0,0,1,17,3.54a4.2,4.2,0,0,0-1.8-1.4A4.22,4.22,0,0,0,13,2.21a4.24,4.24,0,0,1-1.94,0,4.22,4.22,0,0,0-2.24-.07A4.2,4.2,0,0,0,7,3.54a5.11,5.11,0,0,1-.66.66,4.77,4.77,0,0,1-.85.43A4.25,4.25,0,0,0,3.61,5.91,4.21,4.21,0,0,0,3,8.07A4,4,0,0,1,2.82,9a4.57,4.57,0,0,1-.42.82A4.3,4.3,0,0,0,1.63,12a4.3,4.3,0,0,0,.77,2.16,4,4,0,0,1,.42.82,4.11,4.11,0,0,1,.15.95,4.19,4.19,0,0,0,.64,2.16,4.25,4.25,0,0,0,1.87,1.28,4.77,4.77,0,0,1,.85.43,5.11,5.11,0,0,1,.66.66,4.12,4.12,0,0,0,1.8,1.4,3,3,0,0,0,.87.13A6.66,6.66,0,0,0,11,21.81a4,4,0,0,1,1.94,0,4.33,4.33,0,0,0,2.24.06,4.12,4.12,0,0,0,1.8-1.4,5.11,5.11,0,0,1,.66-.66,4.77,4.77,0,0,1,.85-.43,4.25,4.25,0,0,0,1.87-1.28A4.19,4.19,0,0,0,21,15.94a4.11,4.11,0,0,1,.15-.95,4.57,4.57,0,0,1,.42-.82A4.3,4.3,0,0,0,22.37,12,4.3,4.3,0,0,0,21.6,9.84Z" style="fill:#179cf0"/></svg>' : ''}
+                            ${info.seksioni === 'ΚΑΘ' ? '<svg style="margin-left: 5px" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" id="verified" class="icon glyph"><path d="M21.6,9.84A4.57,4.57,0,0,1,21.18,9,4,4,0,0,1,21,8.07a4.21,4.21,0,0,0-.64-2.16,4.25,4.25,0,0,0-1.87-1.28,4.77,4.77,0,0,1-.85-.43A5.11,5.11,0,0,1,17,3.54a4.2,4.2,0,0,0-1.8-1.4A4.22,4.22,0,0,0,13,2.21a4.24,4.24,0,0,1-1.94,0,4.22,4.22,0,0,0-2.24-.07A4.2,4.2,0,0,0,7,3.54a5.11,5.11,0,0,1-.66.66,4.77,4.77,0,0,1-.85.43A4.25,4.25,0,0,0,3.61,5.91,4.21,4.21,0,0,0,3,8.07,4,4,0,0,1,2.82,9a4.57,4.57,0,0,1-.42.82A4.3,4.3,0,0,0,1.63,12a4.3,4.3,0,0,0,.77,2.16,4,4,0,0,1,.42.82,4.11,4.11,0,0,1,.15.95,4.19,4.19,0,0,0,.64,2.16,4.25,4.25,0,0,0,1.87,1.28,4.77,4.77,0,0,1,.85.43,5.11,5.11,0,0,1,.66.66,4.12,4.12,0,0,0,1.8,1.4,3,3,0,0,0,.87.13A6.66,6.66,0,0,0,11,21.81a4,4,0,0,1,1.94,0,4.33,4.33,0,0,0,2.24.06,4.12,4.12,0,0,0,1.8-1.4,5.11,5.11,0,0,1,.66-.66,4.77,4.77,0,0,1,.85-.43,4.25,4.25,0,0,0,1.87-1.28A4.19,4.19,0,0,0,21,15.94a4.11,4.11,0,0,1,.15-.95,4.57,4.57,0,0,1,.42-.82A4.3,4.3,0,0,0,22.37,12,4.3,4.3,0,0,0,21.6,9.84Zm-4.89.87-5,5a1,1,0,0,1-1.42,0l-3-3a1,1,0,1,1,1.42-1.42L11,13.59l4.29-4.3a1,1,0,0,1,1.42,1.42Z" style="fill:#179cf0"/></svg>' : ''}
                             </p>
                         </div>
                         <div class="postContent">
