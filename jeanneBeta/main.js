96ae26535eddbb412cf1dff26132a34e053e891e
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2025-05-07 21:42:22 +0300

    Vapid Changes

--- jeanneBeta/main.js
+++ jeanneBeta/main.js
@@ -37,7 +37,7 @@ window.fetch = async function (input, init = {}) {
         handleRateLimit();
     }
     if (method === 'GET' && response.status === 403) {
-        
+
         const clonedResponse = response.clone();
         const text = await clonedResponse.text();
         console.log('Response text:', text);
@@ -6584,7 +6584,7 @@ function notificationsStart(ready) {
         const registration = await navigator.serviceWorker.ready;
         const subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey: 'BA15u7YIY1VPm9ulrTmaG_dTL1tJj59pso6K46lc2i45u-r1bmdl1t6KOrHxMmzyn8ZDQelik0mGn_blW9gAhg4'
+            applicationServerKey: 'BBoqxxWQqXtAublhzxhuDX5CtQqLFE5OUkGlA9Ezsyae6XYepxyn94FL8y4rR7mF7MH06OBIK9OFX-Z9YDKRtB4'
         });
         console.log("Push Subscription:", JSON.stringify(subscription));
 
@@ -7088,3 +7088,4 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 revertAlphaBackground()
+
